f5a412d2e1599e7c18e289f3609fa2c7c72b74cc
Guilherme Figueiredo <guilhermefigueiredo341@gmail.com>
2024-12-01 10:42:32 -0300

    LibriSpeech: flac2wav

---
+++ LibriSpeech/flac2wav.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+from multiprocessing import Pool, cpu_count
+from pydub import AudioSegment
+
+def convert_flac_to_wav(file_path):
+    """
+    Convert a .flac file to .wav.
+    
+    Args:
+        file_path (str): Full path of the .flac file.
+    """
+    try:
+        wav_path = file_path.with_suffix('.wav')
+        audio = AudioSegment.from_file(file_path, format="flac")
+        audio.export(wav_path, format="wav")
+        #print(f"Convertido: {file_path} -> {wav_path}")
+    except Exception as e:
+        print(f"Error converting {file_path}: {e}")
+
+def process_folder(root_folder):
+    """
+    Finds all .flac files in the directory and converts to .wav using multiple processes.
+
+    Args:
+        root_folder (str): Root directory path.
+    """
+    root_folder = Path(root_folder)
+
+    flac_files = list(root_folder.rglob("*.flac"))
+    if not flac_files:
+        print("No .flac files found.")
+        return
+
+    print(f"Number of .flac files found: {len(flac_files)}")
+
+    with Pool(cpu_count()) as pool:
+        pool.map(convert_flac_to_wav, flac_files)
+
+if __name__ == "__main__":
+    process_folder(os.getcwd())
